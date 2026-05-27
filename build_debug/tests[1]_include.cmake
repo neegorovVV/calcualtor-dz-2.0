285175e9389f67a5562c2e4c75efdead26a6a323
neegorovVV <kolakolaegorov@yandex.ru>
2026-05-27 13:57:12 +0000

    cleanup: remove build artifacts and perf files

--- build_debug/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/test182/calculator/build_debug/tests[1]_tests.cmake")
-  include("/home/test182/calculator/build_debug/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
